e467588968566800717b4ab3c17dee8d3749c434
dmiv <dmitriy40647274@gmail.com>
2022-05-12 23:49:57 +0300

    add autoupdate chart, from database

--- static/js/pages/dashboard-sale-new.js
+++ static/js/pages/dashboard-sale-new.js
@@ -38,7 +38,8 @@ var xValues =  [
           '19 июня 13:57',
           '19 июня 20:42',
         ];
-new Chart("myChart", {
+
+var myChart = new Chart("myChart", {
   type: "line",
     backgroundColor: 'rgba(251, 85, 85, 0.4)',
   data: {
@@ -117,9 +118,7 @@ new Chart("myChart", {
       backgroundColor: "rgb(0,0,255)",
       borderColor: "rgb(0,0,255)",
       data: ai10
-    },
-
-    ]
+    },]
   },
   options: {
     legend: {display: true},
@@ -135,3 +134,103 @@ new Chart("myChart", {
     }
   }
 });
+
+function updatechart(chart, labels, ai1, ai2, ai3, ai4, ai5, ai6, ai7, ai8, ai9, ai10) {
+    chart.data.labels = labels
+    chart.data.datasets = [{
+      label: 'ai1',
+      fill: false,
+      lineTension: 0,
+      backgroundColor: "rgb(255, 0, 0)",
+      borderColor: "rgb(255,0,0)",
+      data: ai1,
+
+    },
+        {
+      label: 'ai2',
+      fill: false,
+      lineTension: 0,
+      backgroundColor: "rgb(255, 156, 0)",
+      borderColor: "rgb(255, 156, 0)",
+      data: ai2
+    },
+        {
+      label:'ai3',
+      fill: false,
+      lineTension: 0,
+      backgroundColor: "rgb(255, 255, 0)",
+      borderColor: "rgb(255, 255, 0)",
+      data: ai3
+    },
+        {
+      label: 'ai4',
+      fill: false,
+      lineTension: 0,
+      backgroundColor: "rgb(215, 179, 104)",
+      borderColor: "rgb(215, 179, 104)",
+      data: ai4
+    },           {
+      label: 'ai5',
+      fill: false,
+      lineTension: 0,
+      backgroundColor: "rgb(156, 117, 154)",
+      borderColor: "rgb(156, 117, 154)",
+      data: ai5
+    },           {
+      label: 'ai6',
+      fill: false,
+      lineTension: 0,
+      backgroundColor: "rgb(0, 105, 65)",
+      borderColor: "rgb(0, 105, 65)",
+      data: ai6
+    },           {
+      label: 'ai7',
+      fill: false,
+      lineTension: 0,
+      backgroundColor: "rgb(119, 118, 231)",
+      borderColor: "rgb(119, 118, 231)",
+      data: ai7
+    },           {
+      label: 'ai8',
+      fill: false,
+      lineTension: 0,
+      backgroundColor: "rgb(178, 118, 231)",
+      borderColor: "rgb(178, 118, 231)",
+      data: ai8
+    },           {
+      label: 'ai9',
+      fill: false,
+      lineTension: 0,
+      backgroundColor: "rgb(167, 184, 136)",
+      borderColor: "rgb(167, 184, 136)",
+      data: ai9
+    },           {
+      label: 'ai10',
+      fill: false,
+      lineTension: 0,
+      backgroundColor: "rgb(0,0,255)",
+      borderColor: "rgb(0,0,255)",
+      data: ai10
+    },];
+    chart.update();
+}
+
+
+function getStatus(taskID) {
+  $.ajax({
+    url: `chart/`,
+    method: 'GET'
+  })
+                        .done((res) => {
+                            // alert(res.ai2);
+                            // alert(ai2);
+updatechart(myChart, res.values, res.ai1, res.ai2, res.ai3, res.ai4, res.ai5, res.ai6, res.ai7, res.ai8, res.ai9, res.ai10);
+setTimeout(function() {
+getStatus(res.task_id);
+}, 30000);
+})
+  .fail((err) => {
+    console.log(err)
+  });
+}
+getStatus(1)
--- tables/views.py
+++ tables/views.py
@@ -17,22 +17,23 @@ def table(request):
 
 def chart(request):
     model = Obj1Cmn
+    limited = 10
 
-    values = model.objects.values_list('date', flat=True)
+    values = model.objects.values_list('date', flat=True)[:limited]
     values = [i.strftime('%d %B %H:%M') for i in values]
     data = {
      # 'values': list(model.objects.values_list('date', flat=True)),
      'values': values,
-        'ai1': list(model.objects.values_list('ai1', flat=True)),
-        'ai2': list(model.objects.values_list('ai2', flat=True)),
-        'ai3': list(model.objects.values_list('ai3', flat=True)),
-        'ai4': list(model.objects.values_list('ai4', flat=True)),
-        'ai5': list(model.objects.values_list('ai5', flat=True)),
-        'ai6': list(model.objects.values_list('ai6', flat=True)),
-        'ai7': list(model.objects.values_list('ai7', flat=True)),
-        'ai8': list(model.objects.values_list('ai8', flat=True)),
-        'ai9': list(model.objects.values_list('ai9', flat=True)),
-        'ai10': list(model.objects.values_list('ai10', flat=True)),
+        'ai1': list(model.objects.values_list('ai1', flat=True)[:limited]),
+        'ai2': list(model.objects.values_list('ai2', flat=True)[:limited]),
+        'ai3': list(model.objects.values_list('ai3', flat=True)[:limited]),
+        'ai4': list(model.objects.values_list('ai4', flat=True)[:limited]),
+        'ai5': list(model.objects.values_list('ai5', flat=True)[:limited]),
+        'ai6': list(model.objects.values_list('ai6', flat=True)[:limited]),
+        'ai7': list(model.objects.values_list('ai7', flat=True)[:limited]),
+        'ai8': list(model.objects.values_list('ai8', flat=True)[:limited]),
+        'ai9': list(model.objects.values_list('ai9', flat=True)[:limited]),
+        'ai10': list(model.objects.values_list('ai10', flat=True)[:limited]),
     }
     # return render(request, 'tables/chart.html', {'data': data})
     return JsonResponse(data)
