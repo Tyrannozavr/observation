3aaa849340e95642627d044d168958760fc41313
dmiv <dmitriy40647274@gmail.com>
2022-05-18 22:40:39 +0300

    all are ready

--- tables/views.py
+++ tables/views.py
@@ -21,19 +21,20 @@ def available_model(request, Cmn=True):
         user = request.session.get('user', 'Cmn001')
     else:
         user = request.user.username
-    # print(models[user])
     return models[user]
 
 def count_error(ai, model=Obj1Ai):
     return model.objects.filter(id_ai=ai, err__gt=0, sts=1).count()
 
-@login_required(login_url='authenticate/')
+@login_required(login_url='/authenticate/')
 def count_error_double(request):
     ai = request.GET.get('ai', False)
     errors = int(count_error(ai))
     return JsonResponse({'errors': errors})
 
-@login_required(login_url='authenticate/')
+
+# real first page
+@login_required(login_url='/authenticate/')
 def index(request):
     if request.POST:
         if bool(request.POST.get('logout', False)):
@@ -41,13 +42,16 @@ def index(request):
             return redirect('authenticate/')
         if request.user.is_staff and request.POST.get('user', False):
             request.session['user'] = request.POST.get('user')
-    check_user = request.session.get('user', None)
+    check_user = request.session.get('user', 'Cmn001')
     users = User.objects.filter(is_staff=False)
     available_model(request)
-    model = Obj1Ai
+    # model = Obj1Ai
+    model = available_model(request, Cmn=False)
+    # print(model)
     sensors =set(model.objects.values_list('id_ai', flat=True))
     errors = (count_error(ai, model) for ai in sensors)
     sensors_errors = list(zip(sensors, errors))
+    print()
     return render(request, 'tables/index.html', {'now': time, 'sensors_errors': sensors_errors, 'users': users, 'check_user': check_user})
 
 def auth_user(request):
@@ -61,10 +65,11 @@ def auth_user(request):
     return render(request, 'authenticate/auth-signin.html')
 
 
-@login_required(login_url='authenticate/')
+@login_required(login_url='/authenticate/')
 def table(request):
     sorted = bool(request.GET.get('sorted', True))
-    model = Obj1Ai
+    # model = Obj1Ai
+    model = available_model(request, Cmn=False)
     if sorted:
         data = model.objects.filter(err__gt=0, sts=1).order_by('-datain')[:20]
     else:
@@ -72,9 +77,10 @@ def table(request):
     return render(request, 'tables/table.html', {'data': data})
 
 
-@login_required(login_url='authenticate/')
+@login_required(login_url='/authenticate/')
 def chart(request):
-    model = Obj1Cmn
+    # model = Obj1Cmn
+    model = available_model(request)
     limit = 240
     if request.session.get('filter', False):
         limit = request.session['filter']
@@ -91,9 +97,10 @@ def chart(request):
     data['titles'] = titles
     return JsonResponse(data)
 
-@login_required(login_url='authenticate/')
+@login_required(login_url='/authenticate/')
 def confirm(request):
-    model = Obj1Ai
+    # model = Obj1Ai
+    model = available_model(request, Cmn=False)
     id = request.GET.get('id')
     obj = model.objects.get(id=id)
     obj.sts = 2
@@ -101,32 +108,42 @@ def confirm(request):
     return redirect('/')
 
 
-@login_required(login_url='authenticate/')
+@login_required(login_url='/authenticate/')
 def filter(request):
     request.session['ai'] = request.GET.get('ai')
     return redirect('/')
 
 
-@login_required(login_url='authenticate/')
+@login_required(login_url='/authenticate/')
 def count_chart(request):
     request.session['filter'] = int(request.GET.get('count', None))
     return redirect('/')
 
-
-@login_required(login_url='authenticate/')
+# second page with detail information
+@login_required(login_url='/authenticate/')
 def detail(request, ai):
+    if request.POST:
+        if bool(request.POST.get('logout', False)):
+            logout(request)
+            return redirect('/authenticate/')
+        if request.user.is_staff and request.POST.get('user', 'Cmn001'):
+            request.session['user'] = request.POST.get('user')
+    check_user = request.session.get('user', None)
+    users = User.objects.filter(is_staff=False)
     now = time
-    model = Obj1Ai
+    # model = Obj1Ai
+    model = available_model(request, Cmn=False)
     filters =set(model.objects.values_list('id_ai', flat=True))
     sensors =set(model.objects.values_list('id_ai', flat=True))
     errors = (count_error(ai, model) for ai in sensors)
     sensors_errors = list(zip(sensors, errors))
-    return render(request, 'tables/detail.html', {'now': now, 'filters': filters, 'sensors_errors': sensors_errors})
+    return render(request, 'tables/detail.html', {'now': now, 'filters': filters, 'sensors_errors': sensors_errors, 'check_user': check_user, 'users': users})
 
 
-@login_required(login_url='authenticate/')
+@login_required(login_url='/authenticate/')
 def detail_chart(request, ai):
-    model = Obj1Ai
+    # model = Obj1Ai
+    model = available_model(request, Cmn=False)
     limit = 240
     if request.session.get('detail_count', False):
         limit = request.session['detail_count']
@@ -144,31 +161,43 @@ def detail_chart(request, ai):
     }
     return JsonResponse(data)
 
-@login_required(login_url='authenticate/')
+@login_required(login_url='/authenticate/')
 def detail_count(request):
     request.session['detail_count'] = int(request.GET.get('detail_count'))
     return redirect('/')
 
 
-@login_required(login_url='authenticate/')
+@login_required(login_url='/authenticate/')
 def detail_filter(request):
     request.session['detail_filter'] = int(request.GET.get('ai'))
     return redirect('/')
 
-@login_required(login_url='authenticate/')
+@login_required(login_url='/authenticate/')
 def detail_table(request, ai):
-    model = Obj1Ai
+    # model = Obj1Ai
+    model = available_model(request, Cmn=False)
     if ai and ai != 'None':
         data = model.objects.filter(err__gt=0, sts=1, id_ai=ai).order_by('-datain')[:20]
     else:
         data = model.objects.filter(err__gt=0, sts=1).order_by('-datain')[:20]
     return render(request, 'tables/table.html', {'data': data})
 
-@login_required(login_url='authenticate/')
+
+# page archive
+@login_required(login_url='/authenticate/')
 def archive(request):
+    if request.POST:
+        if bool(request.POST.get('logout', False)):
+            logout(request)
+            return redirect('/authenticate/')
+        if request.user.is_staff and request.POST.get('user', 'Cmn001'):
+            request.session['user'] = request.POST.get('user')
+    check_user = request.session.get('user', None)
+    users = User.objects.filter(is_staff=False)
     now = time
-    model = Obj1Ai
+    # model = Obj1Ai
+    model = available_model(request, Cmn=False)
     sensors =set(model.objects.values_list('id_ai', flat=True))
     errors = (count_error(ai, model) for ai in sensors)
     sensors_errors = list(zip(sensors, errors))
-    return render(request, 'tables/archive.html', {'now': now, 'sensors_errors': sensors_errors})
+    return render(request, 'tables/archive.html', {'now': now, 'sensors_errors': sensors_errors, 'check_user': check_user, 'users': users})
